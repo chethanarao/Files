ed8dc4fd2a92fe55cd05c2dcd549e27ea2f615f0
chethanarao <79127754+chethanarao@users.noreply.github.com>
2021-02-25 17:59:45 +0530

    Update updateemployee1.c

--- updateemployee1.c
+++ updateemployee1.c
@@ -15,18 +15,22 @@ int main()
     printf("enter employee id to search\n");
     scanf("%d",&id);
     fseek(fp,0,SEEK_SET);
-    for(i=0;i<=3;i++)
-    {
-     fread(&emp1[i],sizeof(emp1),1,fp);
-            if(emp1[i].empid==id)
-            {
-                printf("enter the new data\n");
-                scanf("%d %s %s",&emp1[i].empid,&emp1[i].empname,&emp1[i].company);
-                fseek(fp,sizeof(emp1[i])*i,SEEK_SET);
-                fwrite(&emp1[i],sizeof(emp1),1,fp);
-                break;
-            }
-        }
+    
+     while(fread(&emp1[i],sizeof(emp1),1,fp)==1)
+     {
+          for(i=0;i<=3;i++)
+          {
+                 if(emp1[i].empid==id)
+                 {
+                     printf("enter the new data\n");
+                     scanf("%d %s %s",&emp1[i].empid,&emp1[i].empname,&emp1[i].company);
+                     fseek(fp,sizeof(emp1[i])*i,SEEK_SET);
+                     fwrite(&emp1[i],sizeof(emp1),1,fp);
+                     break;
+                  }
+           }
+       }
+         
      printf("updated ");
         fclose(fp);
     }
