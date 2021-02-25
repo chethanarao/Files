06bf1ca783178fbb8047e2aaba7909d1e2719293
chethanarao <79127754+chethanarao@users.noreply.github.com>
2021-02-25 14:05:07 +0530

    Create searchemp.c
    hhj

---
+++ searchemp.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+struct emp
+{
+     int empid;
+     char empname[10];
+     char company[10];
+     
+}emp1[10];
+
+int main()
+{
+    int i,id;
+    FILE *fp;
+    fp=fopen("test","rb");
+    printf("enter employee id to search\n");
+    scanf("%d",&id);
+    while(fread(&emp1[i],sizeof(emp1),1,fp)==1)
+    {
+        for(i=0;i<3;i++)
+        {
+            if(emp1[i].empid==id)
+            {
+                flag=1;
+                printf("found");
+                printf("%s %s",emp1[i].empname,emp1[i].company);
+            }
+        }
+        if(flag==0)
+        {
+            printf("not found");
+        }
+    }
