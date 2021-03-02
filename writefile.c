0bbc42922d56fef8d158155bbd59e066a32bd3d8
chethanarao <79127754+chethanarao@users.noreply.github.com>
2021-03-02 12:45:36 +0530

    Update writefile.c
    ftft

--- writefile.c
+++ writefile.c
@@ -1,17 +1,16 @@
-#include<stdio.h>
-struct node
+#include <stdio.h> #include <string.h> struct node
 {
-    int data;
-    struct node *link;
-};typedef struct node ll;
-int main()
-{
-    FILE *fp; int ll n1;
-    fp=fopen("file","wb");
-    n1.data=10;
-    n1.link=12345;
-    fwrite(n1,sizeof(n1),1,fp);
-    printf("done writing to file\n");
+int data;
+
+struct node *link;
+};
+typedef struct node ll; int main () {
+FILE *fp;
+fp = fopen("data", "wb");
+    ll n1;
+n1.data = 100 ; 
+    n1.link = 12345;
+fwrite(n1, sizeof(n1), 1, fp); printf("Done writing to file "); 
     fclose(fp);
-    
+return(0);
 }
