7723fa22bc661132181518c67f4f054aa9d4e1a2
chethanarao <79127754+chethanarao@users.noreply.github.com>
2021-03-02 12:41:28 +0530

    Update readfile.c
    gjyh

--- readfile.c
+++ readfile.c
@@ -8,13 +8,7 @@ int main()
 {
     FILE *fp;
     fp=fopen("file.txt","r");
-    for(i=0;i<3;i++)
-{
     fread(&obj[i],sizeof(obj),1,fp);
-}
-for(i=0;i<3;i++)
-{
     printf("%d %s",obj[i].data,obj[i].name);
-}
 fclose(fp);
 }
