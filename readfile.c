e85fa361db2ca02e87bed6d2706dc9a80a9ce9d7
chethanarao <79127754+chethanarao@users.noreply.github.com>
2021-03-02 12:53:08 +0530

    Update readfile.c
    sxax

--- readfile.c
+++ readfile.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
-struct employee
-{
-    int data;
-    char name[10];
-}obj[10];
-int main()
-{
-    FILE *fp;
-    fp=fopen("file.txt","r");
-    fread(&obj[i],sizeof(obj),1,fp);
-    printf("%d %s",obj[i].data,obj[i].name);
-fclose(fp);
-}
+struct node
+{ int data; 
+ struct node *link;
+}; 
+typedef struct node ll;
+int main ()
+{ 
+    FILE *fp; 
+ ll n2;// empty node 
+    fp = fopen("data", "rb");
+    fseek(fp, 0, SEEK_SET);
+    fread(&n2, sizeof(n2), 1, fp);// fill n2 from file
+    printf("%d",n2.data);
+    printf("%u",n2.link);
+    fclose(fp);
+    return(0); }
